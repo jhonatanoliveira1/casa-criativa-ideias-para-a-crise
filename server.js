80605d36870c13929257a800b6f930a7547b1976
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-05-13 10:33:27 -0300

    Front-end da aplicação

--- server.js
+++ server.js
@@ -2,60 +2,64 @@
 const express = require("express")
 const server = express()
 
-const ideas = [
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-        title: "Cursos de Programação",
-        category: "Estudo",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. harum illum voluptates repudiandae explicabo asperiores.",
-        url: "http://rocketseat.com.br"
-    },
+const db = require("./db")
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-        title: "Exercícios",
-        category: "Saúde",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. harum illum voluptates repudiandae explicabo asperiores.",
-        url: "http://rocketseat.com.br"
-    },
+// const ideas = [
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+//         title: "Cursos de Programação",
+//         category: "Estudo",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. harum illum voluptates repudiandae explicabo asperiores.",
+//         url: "http://rocketseat.com.br"
+//     },
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-        title: "Meditação",
-        category: "Mentalidade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. harum illum voluptates repudiandae explicabo asperiores.",
-        url: "http://rocketseat.com.br"
-    },
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+//         title: "Exercícios",
+//         category: "Saúde",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. harum illum voluptates repudiandae explicabo asperiores.",
+//         url: "http://rocketseat.com.br"
+//     },
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-        title: "Karaokê",
-        category: "Diversão em família",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. harum illum voluptates repudiandae explicabo asperiores.",
-        url: "http://rocketseat.com.br"
-    },
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+//         title: "Meditação",
+//         category: "Mentalidade",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. harum illum voluptates repudiandae explicabo asperiores.",
+//         url: "http://rocketseat.com.br"
+//     },
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
-        title: "Pintura",
-        category: "Criatividade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. harum illum voluptates repudiandae explicabo asperiores.",
-        url: "http://rocketseat.com.br"
-    },
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+//         title: "Karaokê",
+//         category: "Diversão em família",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. harum illum voluptates repudiandae explicabo asperiores.",
+//         url: "http://rocketseat.com.br"
+//     },
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
-        title: "Recortes",
-        category: "Criatividade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. harum illum voluptates repudiandae explicabo asperiores.",
-        url: "http://rocketseat.com.br"
-    }
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
+//         title: "Pintura",
+//         category: "Criatividade",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. harum illum voluptates repudiandae explicabo asperiores.",
+//         url: "http://rocketseat.com.br"
+//     },
 
-]
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
+//         title: "Recortes",
+//         category: "Criatividade",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. harum illum voluptates repudiandae explicabo asperiores.",
+//         url: "http://rocketseat.com.br"
+//     }
 
+// ]
 
 // configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
+
+// habilitar uso do req.body
+server.use(express.urlencoded({ extended: true }))
 
 // configuração do nunjucks
 const nunjucks =require("nunjucks")
@@ -68,23 +72,69 @@ nunjucks.configure("views", {
 // e capturo o pedido do cliente para responder
 server.get("/", function(req, res) {
 
-    const reverseIdeas = [...ideas].reverse()
-
-    let lastIdeas = []
-    for (let idea of ideas) {
-        if(lastIdeas.length < 2) {
-            lastIdeas.push(idea)
+    db.all(`SELECT * FROM ideas`, function(err, rows) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
         }
-    }
 
-    return res.render("index.html", {ideas: lastIdeas})
+        const reverseIdeas = [...rows].reverse()
+    
+        let lastIdeas = []
+        for (let idea of reverseIdeas) {
+            if(lastIdeas.length < 2) {
+                lastIdeas.push(idea)
+            }
+        }
+    
+        return res.render("index.html", {ideas: lastIdeas})
+    })
+
 })
 
 server.get("/ideias", function(req, res) {
 
-    const reverseIdeas = [...ideas].reverse()
+    db.all(`SELECT * FROM ideas`, function(err, rows) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
 
-    return res.render("ideias.html", {ideas: reverseIdeas})
+        const reverseIdeas = [...rows].reverse()
+    
+        return res.render("ideias.html", {ideas: reverseIdeas})
+    })
+
+})
+
+server.post("/", function(req, res){
+
+    //Inserir dados na tabela
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES (?,?,?,?,?);
+    `
+    const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link
+    ]
+
+    db.run(query, values, function(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
+
+        return res.redirect("/ideias")
+    })
 })
 
 //liguei meu servidor na porta 3000
